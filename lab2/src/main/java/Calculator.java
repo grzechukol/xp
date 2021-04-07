3f68e6bd0632f404c34566fa6c96f34ea6acfe5c
Michał Pałys <michal.palys.konto@gmail.com>
2021-04-07 21:49:44 +0200

    first kata

--- lab2/src/main/java/Calculator.java
+++ lab2/src/main/java/Calculator.java
@@ -1,19 +1,29 @@
+import java.util.Arrays;
+
 public class Calculator {
- public int Add(String numbers) {
-  var splitNumbers = numbers.split(",");
+ public int add(String numbers) throws Exception {
+  var splitRegEx = ",|\n";
 
-  if (splitNumbers.length == 0){
-   return 0;
+  if (numbers.startsWith("//")) {
+   var splitOnFirstNewLine = numbers.split("\n", 2);
+   var customDelimiter = splitOnFirstNewLine[0].replace("//", "");
+   splitRegEx += "|" + customDelimiter;
+   numbers = splitOnFirstNewLine[1];
   }
 
-  if (splitNumbers.length == 1){
-   if(splitNumbers[0].length() == 0) {
-    return 0;
-   }
+  var splitedNumbers = Arrays.stream(numbers.split(splitRegEx, -1))
+          .filter(item -> !item.isEmpty())
+          .mapToInt(Integer::parseInt)
+          .toArray();
 
-   return Integer.parseInt(splitNumbers[0]);
+  var negatives = Arrays.stream(splitedNumbers).filter(x -> x < 0)
+          .mapToObj(String::valueOf)
+          .toArray(String[]::new);
+
+  if (Arrays.stream(negatives).findAny().isPresent()) {
+   throw new Exception("Negatives not allowed: " + String.join(",", negatives));
   }
 
-  return Integer.parseInt(splitNumbers[0]) + Integer.parseInt(splitNumbers[1]);
+  return Arrays.stream(splitedNumbers).reduce(0, Integer::sum);
  }
 }
--- lab2/src/test/java/CalculatorTests.java
+++ lab2/src/test/java/CalculatorTests.java
@@ -7,23 +7,106 @@ import org.junit.runners.Parameterized;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @RunWith(Parameterized.class)
 public class CalculatorTests {
 
- public static Stream<Arguments> provideParameters(){
-  return Stream.of(Arguments.of("", 0), Arguments.of("1", 1), Arguments.of("1,2", 3));
+ // 1
+ public static Stream<Arguments> getParams_addsUpToTwoNumber_whenStringIsValid(){
+  return Stream.of(Arguments.of("", 0), Arguments.of("1", 1), Arguments.of("3,2", 5));
  }
 
  @ParameterizedTest
- @MethodSource("provideParameters")
- public void add_addsUpToTwoNumber_whenStringIsValid(String calculation, int expected){
+ @MethodSource("getParams_addsUpToTwoNumber_whenStringIsValid")
+ public void add_addsUpToTwoNumber_whenStringIsValid(String calculation, int expected) throws Exception {
   // Arrange
   var systemUnderTest = new Calculator();
 
   // Act
-  var result = systemUnderTest.Add(calculation);
+  var result = systemUnderTest.add(calculation);
 
   // Assert
   Assertions.assertEquals(expected, result);
+ }
+
+
+ // 2
+ public static Stream<Arguments> getParams_addsUpToAnyNumber_whenStringIsValid(){
+  return Stream.of(Arguments.of("2,3,4", 9), Arguments.of("20,10,90,20", 140));
+ }
+
+ @ParameterizedTest
+ @MethodSource("getParams_addsUpToAnyNumber_whenStringIsValid")
+ public void add_addsUpToAnyNumber_whenStringIsValid(String calculation, int expected) throws Exception {
+  // Arrange
+  var systemUnderTest = new Calculator();
+
+  // Act
+  var result = systemUnderTest.add(calculation);
+
+  // Assert
+  Assertions.assertEquals(expected, result);
+ }
+
+
+ // 3
+ public static Stream<Arguments> getParams_addsNumbersUsingNewLineDelimiter_whenStringIsValid(){
+  return Stream.of(Arguments.of("2\n3\n4", 9), Arguments.of("20\n10,90\n20", 140));
+ }
+
+ @ParameterizedTest
+ @MethodSource("getParams_addsNumbersUsingNewLineDelimiter_whenStringIsValid")
+ public void add_addsNumbersUsingNewLineDelimiter_whenStringIsValid(String calculation, int expected) throws Exception {
+  // Arrange
+  var systemUnderTest = new Calculator();
+
+  // Act
+  var result = systemUnderTest.add(calculation);
+
+  // Assert
+  Assertions.assertEquals(expected, result);
+ }
+
+
+ // 4
+ public static Stream<Arguments> getParams_addsNumbersUsingCustomDelimiter_whenStringIsValid(){
+  return Stream.of(Arguments.of("//;\n2;3;4", 9), Arguments.of("//;\n20\n10,90\n20", 140));
+ }
+
+ @ParameterizedTest
+ @MethodSource("getParams_addsNumbersUsingCustomDelimiter_whenStringIsValid")
+ public void add_addsNumbersUsingCustomDelimiter_whenStringIsValid(String calculation, int expected) throws Exception {
+  // Arrange
+  var systemUnderTest = new Calculator();
+
+  // Act
+  var result = systemUnderTest.add(calculation);
+
+  // Assert
+  Assertions.assertEquals(expected, result);
+ }
+
+
+ // 5
+ public static Stream<Arguments> getParams_shouldThrowException_whenNegativeNumber(){
+  return Stream.of(Arguments.of("//;\n-2;3;4", "-2"));
+ }
+
+ @ParameterizedTest
+ @MethodSource("getParams_shouldThrowException_whenNegativeNumber")
+ public void add_shouldThrowException_whenNegativeNumber(String calculation, String negativeNumbers)
+ {
+  // Arrange
+  var systemUnderTest = new Calculator();
+
+  // Act
+  Exception exception = assertThrows(Exception.class, () -> systemUnderTest.add(calculation));
+  String expectedMessage = "Negatives not allowed: " + negativeNumbers;
+  String actualMessage = exception.getMessage();
+
+  // Assert
+  assertTrue(actualMessage.contains(expectedMessage));
  }
 }
