d9a11496bbe8631c94617087d1c4363088196324
Fryzu <bartek.fryz@gmail.com>
2021-04-07 20:21:16 +0200

    Adds junit5 dependencies to maven

---
+++ src/src/test/java/SetupTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SetupTests {
+    @Test
+    void testJunitImportedCorrectly() throws Exception {
+        Assertions.assertEquals(true, true);
+    }
+}
