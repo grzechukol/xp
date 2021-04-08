d482dec79b762f8794db7027b73f1f582d9235e6
Fryzu <bartek.fryz@gmail.com>
2021-04-08 20:39:02 +0200

    Chagnes the order of the test assertion arguments

--- src/src/test/java/TDD/CalculatorTests.java
+++ src/src/test/java/TDD/CalculatorTests.java
@@ -17,30 +17,30 @@ public class CalculatorTests {
     @Test
     @DisplayName("Adds up to two numbers")
     void addsTwoNumbers () throws Exception {
-        Assertions.assertEquals(sut.add(""), 0);
-        Assertions.assertEquals(sut.add("1"), 1);
-        Assertions.assertEquals(sut.add("1,2"), 3);
+        Assertions.assertEquals(0, sut.add(""));
+        Assertions.assertEquals(1, sut.add("1"));
+        Assertions.assertEquals(3, sut.add("1,2"));
     }
 
     @Test
     @DisplayName("Adds up to any numbers")
     void addsAnyNumbers () throws Exception  {
-        Assertions.assertEquals(sut.add("1,2,3"), 6);
-        Assertions.assertEquals(sut.add("10,90,10,20"), 130);
+        Assertions.assertEquals(6, sut.add("1,2,3"));
+        Assertions.assertEquals(130, sut.add("10,90,10,20"));
     }
 
     @Test
     @DisplayName("Adds numbers using new line delimiters")
     void addsNumbersWithNewLineDelimiters () throws Exception  {
-        Assertions.assertEquals(sut.add("1\n2,3"), 6);
-        Assertions.assertEquals(sut.add("10\n90,10\n20"), 130);
+        Assertions.assertEquals(6, sut.add("1\n2,3"));
+        Assertions.assertEquals(130, sut.add("10\n90,10\n20"));
     }
 
     @Test
     @DisplayName("Adds numbers using custom delimiters ")
     void addsNumbersWithCustomDelimiters () throws Exception  {
-        Assertions.assertEquals(sut.add("//;\n1;2"), 3);
-        Assertions.assertEquals(sut.add("//;\n1;2;4"), 7);
+        Assertions.assertEquals(3, sut.add("//;\n1;2"));
+        Assertions.assertEquals(7, sut.add("//;\n1;2;4"));
     }
 
     @Test
