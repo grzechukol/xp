e6b0b0c957aaf2bb516588885d67d5e57ee1bfe3
Fryzu <bartek.fryz@gmail.com>
2021-04-08 20:34:26 +0200

    Adds bitCounter implementation

--- src/src/main/java/TDD/BitCounter.java
+++ src/src/main/java/TDD/BitCounter.java
@@ -1,19 +1,33 @@
 package TDD;
 
 public class BitCounter {
-    public static class OutOfBoundsException extends Exception {
-        public OutOfBoundsException(String errorMessage) {
+    private static final int UPPER_BOUND = 255;
+    private static final int LOWER_BOUND = 0;
+
+    public static class NumberOutOfBoundsException extends Exception {
+        public NumberOutOfBoundsException(String errorMessage) {
             super(errorMessage);
         }
     }
 
-    public static class WrongFormatException extends Exception {
-        public WrongFormatException(String errorMessage) {
-            super(errorMessage);
+    public int noOfBits1(String inputString) throws Exception {
+        int cnt = 0;
+        var splitRegex = ";|\\s|\n";
+        var splitNumbers = inputString.split(splitRegex);
+        for (var number : splitNumbers) {
+            if (number.isEmpty()) {
+                cnt += 0;
+            } else {
+                var parsedNumber = number.startsWith("$")
+                        ? Integer.parseInt(number.substring(1), 16)
+                        : Integer.parseInt(number);
+                if (parsedNumber > UPPER_BOUND || parsedNumber < LOWER_BOUND) {
+                    throw new NumberOutOfBoundsException("Number "+number+" is not inside the range ("+LOWER_BOUND+","+UPPER_BOUND+")");
+                }
+                cnt += Integer.bitCount(parsedNumber);
+            }
         }
+        return cnt;
     }
 
-    public int noOfBits1(String numbers) {
-        return 1;
-    }
 }
--- src/src/test/java/TDD/BitCounterTests.java
+++ src/src/test/java/TDD/BitCounterTests.java
@@ -17,43 +17,43 @@ public class BitCounterTests {
     @Test
     @DisplayName("Counts ones in the decimal number")
     void worksForDecimal () throws Exception {
-        Assertions.assertEquals(sut.noOfBits1("255"), 8);
-        Assertions.assertEquals(sut.noOfBits1("1"), 1);
-        Assertions.assertEquals(sut.noOfBits1("112"), 3);
+        Assertions.assertEquals(8, sut.noOfBits1("255"));
+        Assertions.assertEquals(1, sut.noOfBits1("1"));
+        Assertions.assertEquals(3, sut.noOfBits1("112"));
     }
 
     @Test
     @DisplayName("Throws exception for numbers outside range 0-255")
-    void throwsForNumbersOutsideRange () throws Exception {
-        Assertions.assertThrows(BitCounter.OutOfBoundsException.class, ()->sut.noOfBits1("256"));
-        Assertions.assertThrows(BitCounter.OutOfBoundsException.class, ()->sut.noOfBits1("-3"));
+    void throwsForNumbersOutsideRange () {
+        Assertions.assertThrows(BitCounter.NumberOutOfBoundsException.class, ()->sut.noOfBits1("256"));
+        Assertions.assertThrows(BitCounter.NumberOutOfBoundsException.class, ()->sut.noOfBits1("-3"));
     }
 
     @Test
     @DisplayName("Counts ones for multiple number split with semicolon")
     void worksForMultipleNumbers () throws Exception {
-        Assertions.assertEquals(sut.noOfBits1("255;1;112"), 12);
-        Assertions.assertEquals(sut.noOfBits1("1;112"), 4);
+        Assertions.assertEquals(12, sut.noOfBits1("255;1;112"));
+        Assertions.assertEquals(4, sut.noOfBits1("1;112"));
     }
 
     @Test
     @DisplayName("Counts ones for multiple number split with whitespace characters")
     void worksForMultipleNumbersWhitespace () throws Exception {
-        Assertions.assertEquals(sut.noOfBits1("255 1    112"), 12);
-        Assertions.assertEquals(sut.noOfBits1("1\n112"), 4);
+        Assertions.assertEquals(12, sut.noOfBits1("255 1    112"));
+        Assertions.assertEquals(4, sut.noOfBits1("1\n112"));
     }
 
     @Test
-    @DisplayName("Throws exception for numbers outside range 0-255")
-    void throwsForWrongFormat () throws Exception {
-        Assertions.assertThrows(BitCounter.WrongFormatException.class, ()->sut.noOfBits1("1 something_weird 23"));
+    @DisplayName("Throws exception for badly formatted input")
+    void throwsForWrongFormat () {
+        Assertions.assertThrows(IllegalArgumentException.class, ()->sut.noOfBits1("1 something_weird 23"));
     }
 
     @Test
     @DisplayName("Counts ones for hex numbers")
     void worksForHexNumbers () throws Exception {
-        Assertions.assertEquals(sut.noOfBits1("255;$1"), 12);
-        Assertions.assertEquals(sut.noOfBits1("1;$70"), 4);
+        Assertions.assertEquals(9, sut.noOfBits1("255;$1"));
+        Assertions.assertEquals(4, sut.noOfBits1("1;$70"));
     }
 
 }
