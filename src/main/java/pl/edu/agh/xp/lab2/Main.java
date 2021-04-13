46bae2bfb838f799e569df0d98d6beef826dfa55
dominik.gnatek <dominikjang@gmail.com>
2021-04-13 18:28:40 +0200

    Simple test main

---
+++ src/main/java/pl/edu/agh/xp/lab2/Main.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.xp.lab2;
+
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.print("Main works");
+
+    }
+}
