c84c86e6d8995649346f0357da233e48d3d4b689
Grzegorz Kunc <grzeg.kunc@gmail.com>
2021-04-21 14:22:35 +0200

    Add custom exception

---
+++ TDD/src/main/java/com/calculator/NegativeNumberException.java
@@ -0,0 +1,7 @@
+package com.calculator;
+
+public class NegativeNumberException  extends Exception {
+    public NegativeNumberException(String errorMessage) {
+        super(errorMessage);
+    }
+}
