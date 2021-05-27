6f4951de16ed9dad8e5c05f2d0cdd0323ce2a137
Grzegorz Kunc <grzeg.kunc@gmail.com>
2021-05-27 23:37:46 +0200

    Add UT for views

--- src/test/java/com/actions/NotFoundActionTests.java
+++ src/test/java/com/actions/NotFoundActionTests.java
@@ -1,18 +1,19 @@
 package com.actions;
 
+import com.ExitAssertions;
 import com.views.NotFoundView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NotFoundActionTests {
-//    @BeforeEach
-//    public void Setup() {
-//
-//    }
-//
-//    @Test
-//    public void Invoke_Should() {
-//        var sut = new NotFoundView();
-//        sut.invoke("Action not found");
-//    }
+    @Test
+    public void Inovke_ShouldReturnFalse_WhenActionIsNotFound() {
+        var sut = new NotFoundAction();
+        NotFoundView notFoundView = Mockito.spy(new NotFoundView());
+        var result = sut.invoke("other");
+        assertEquals(false, result);
+    }
 }
