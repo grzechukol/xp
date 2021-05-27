90fc1e646a04b49271bc523848ab704c8f23fccf
dominik.gnatek <dominikjang@gmail.com>
2021-05-27 22:15:15 +0200

    Repairing JUnit in pom

--- src/test/java/com/actions/ExitActionTests.java
+++ src/test/java/com/actions/ExitActionTests.java
@@ -1,23 +1,20 @@
-//package com.actions;
-//
-//import org.junit.Rule;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.contrib.java.lang.system.ExpectedSystemExit;
-//
-//public class ExitActionTests {
-//    @Rule
-//    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
-//
-//    @BeforeEach
-//    public void Setup() {
-//
-//    }
-//
-//    @Test
-//    public void Invoke_Should() {
-//        var sut = new ExitAction();
-//        exit.expectSystemExitWithStatus(1);
-//        sut.invoke("exit");
-//    }
-//}
+package com.actions;
+
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.ClearSystemProperties;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class ExitActionTests {
+    @Rule
+    public final ClearSystemProperties myPropertyIsCleared
+            = new ClearSystemProperties("MyProperty");
+
+    @Test
+    public void overrideProperty() {
+        assertNull(System.getProperty("MyProperty"));
+    }
+
+}
--- src/test/java/com/transactions/TransactionSaverTests.java
+++ src/test/java/com/transactions/TransactionSaverTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.*;
 
 import java.io.*;
 import java.nio.file.FileAlreadyExistsException;
