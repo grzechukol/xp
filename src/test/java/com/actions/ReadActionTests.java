a612322decb8331602164b3fc12612f87302bc4e
Grzegorz Kunc <grzeg.kunc@gmail.com>
2021-06-01 13:03:22 +0200

    Add test for ReadAction

--- src/test/java/com/actions/ReadActionTests.java
+++ src/test/java/com/actions/ReadActionTests.java
@@ -1,4 +1,39 @@
 package com.actions;
 
+import com.views.NotFoundView;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 public class ReadActionTests {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void Inovke_ShouldCallNextAction_WhenActionIsNotRead() throws IOException {
+        ReadAction readActionMock = Mockito.spy(new ReadAction());
+        readActionMock.invoke("other");
+        verify(readActionMock, times(1)).invokeNext("other");
+        assertEquals("", outputStreamCaptor.toString()
+                .trim());
+    }
 }
