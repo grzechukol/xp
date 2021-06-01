348a82119127d762f6d9620b0e46b613ea303e9b
Grzegorz Kunc <grzeg.kunc@gmail.com>
2021-06-01 13:29:20 +0200

    Add test for ReadAction

--- src/test/java/com/actions/ReadActionTests.java
+++ src/test/java/com/actions/ReadActionTests.java
@@ -1,14 +1,15 @@
 package com.actions;
 
+import com.transactions.TransactionReader;
 import com.views.NotFoundView;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
@@ -21,22 +22,56 @@ public class ReadActionTests {
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
+        File testDir = new File("./test");
+        testDir.mkdir();
+
+        var fileName = "./test/file";
+        var expected = new ArrayList<String[]>();
+        expected.add(new String[]{ "1","Transaction1","Transaction1 Description","10-05-2021","EUR" });
+        expected.add(new String[]{ "2", "Transaction2", "Transaction2 Description", "11-05-2021","USD" });
+
+        try {
+            File file = new File(fileName + ".csv");
+            file.createNewFile();
+            Writer fileWriter = new FileWriter(fileName + ".csv", false);
+            var columns = Arrays.asList(
+                    "ID", "Name", "Description", "Date", "Currency"
+            );
+            fileWriter.append(String.join(";", columns));
+            fileWriter.append("\n");
+            fileWriter.append(String.join(";", expected.get(0)));
+            fileWriter.append("\n");
+            fileWriter.append(String.join(";", expected.get(1)));
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
     public void tearDown() {
         System.setOut(standardOut);
+        try {
+            var dir = new File("./test");
+            for (File file : dir.listFiles())
+                file.delete();
+            dir.delete();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void Inovke_ShouldCallReadTransactions_WhenActionRead() throws IOException {
-        // stworzyc nowy testowy plik transakcji
-        // przeczytac go
-        // sprawdzic czy zawartosc sie zgadza
-        // usunac plik
-        ReadAction readActionMock = Mockito.spy(new ReadAction());
-        readActionMock.invoke("read");
-        assertEquals("", outputStreamCaptor.toString()
+        var sut = new ReadAction();
+        var fileName = "./test/file";
+        TransactionReader transactionReader = new TransactionReader(fileName);
+        sut.setTransactionReader(transactionReader);
+        sut.invoke("read");
+        var expected = "Reading transactions...\n" +
+                "[1, Transaction1, Transaction1 Description, 10-05-2021, EUR]\n" +
+                "[2, Transaction2, Transaction2 Description, 11-05-2021, USD]";
+        assertEquals(expected, outputStreamCaptor.toString()
                 .trim());
     }
 
