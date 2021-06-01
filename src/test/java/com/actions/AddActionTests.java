5c1cbd2de16e47333d6f292923939ea5c203bdfb
dominik.gnatek <dominikjang@gmail.com>
2021-06-01 03:11:30 +0200

    Repairing JUnit in pom

--- src/test/java/com/actions/AddActionTests.java
+++ src/test/java/com/actions/AddActionTests.java
@@ -1,17 +1,37 @@
 package com.actions;
 
+import com.ExitAssertions;
+import com.transactions.IncorrectDataException;
+import com.transactions.TransactionSaver;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class AddActionTests {
-    @BeforeEach
-    public void Setup() {
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddActionTests {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @Test
-    public void Invoke_Should() {
+    public void Invoke_ShouldAdd() throws IncorrectDataException, IOException {
+        String input = "1\n" + "1\n" + "1\n" +"1\n" + "1" ;
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
         var sut = new AddAction();
-//        sut.invoke("add");
+        sut.invoke("add");
+        assertEquals("Adding new transaction...\n" + "ID: Name: Description: Date: Currency:",
+                outputStreamCaptor.toString().trim());
     }
 }
