55a8e68c7259e0bf8718f37ccbaf4ae3c75e5b5d
Fryzu <bartek.fryz@gmail.com>
2021-06-15 15:02:59 +0200

    Adds SumUpCalculator

--- src/main/java/com/calculations/SumUpCalculator.java
+++ src/main/java/com/calculations/SumUpCalculator.java
@@ -1,2 +1,15 @@
-package com.calculations;public class SumUpCalculator {
+package com.calculations;
+
+import java.util.List;
+
+public class SumUpCalculator {
+    private List<String[]> transactions;
+
+    public SumUpCalculator(List<String[]> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void printSummary() {
+        System.out.println("teststwts");
+    }
 }
